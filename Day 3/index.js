863fd2108372ddc52b8c22b74d5616886eadb5b0
Crystal <crwainstock@gmail.com>
2023-12-03 08:34:21 +0100

    added notes about challenge 3 in index

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,5 +1,18 @@
 // DAY 3 -- Gear Ratios
 
+// The engineer explains that an engine part seems to be missing from the engine, but nobody can
+// figure out which one. If you can add up all the part numbers in the engine schematic,
+// it should be easy to work out which part is missing.
+
+// The engine schematic (your puzzle input) consists of a visual representation of the engine.
+// There are lots of numbers and symbols you don't really understand, but apparently any number
+// adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum.
+// (Periods (.) do not count as a symbol.)
+
+// 💡 Any number adjacent to symbol is a part number (even diagonally)
+// 💡 Periods are not symbols
+// 💡 Need the sum of the part numbers
+
 // Get data from day3Data
 const fs = require("fs");
 
