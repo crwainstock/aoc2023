8f65f8d6ec747e997dce983dd09b8c7482687049
Crystal <crwainstock@gmail.com>
2023-12-03 08:29:23 +0100

    added day3Data

---
+++ Day 3/index.js
@@ -0,0 +1,7 @@
+// DAY 3 -- Gear Ratios
+
+// Get data from day3Data
+const fs = require("fs");
+
+// PROCESS THE DATA -- SEPARATE INTO LINES, PUT LINES INTO OBJECT WITH KEY/VALUE PAIRS
+const content = fs.readFileSync("day3Data.txt", { encoding: "utf-8" });
