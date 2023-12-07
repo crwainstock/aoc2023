5624863cafbd0647d34f8c425fb920429ad7108d
Crystal <crwainstock@gmail.com>
2023-12-07 11:19:00 +0100

    get day 6 data from txt file

--- Day 6/index.js
+++ Day 6/index.js
@@ -16,3 +16,22 @@
 
 // Time:      7  15   30
 // Distance:  9  40  200
+
+// This document describes three races:
+
+// The first race lasts 7 milliseconds. The record distance in this race is 9 millimeters.
+// The second race lasts 15 milliseconds. The record distance in this race is 40 millimeters.
+// The third race lasts 30 milliseconds. The record distance in this race is 200 millimeters.
+
+// Your toy boat has a starting speed of zero millimeters per millisecond.
+// For each whole millisecond you spend at the beginning of the race holding down the
+// button, the boat's speed increases by one millimeter per millisecond.
+
+const fs = require("fs");
+const content = fs
+  .readFileSync("input.txt", { encoding: "utf-8" })
+  .toString()
+  .replace(/(\r)/gm, "");
+let lines = content.split("\n");
+
+console.log(lines);
