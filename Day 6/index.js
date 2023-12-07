4af475a5ff30dad90687a74a9a044eab30ccb094
Crystal <crwainstock@gmail.com>
2023-12-07 11:02:39 +0100

    add challenge info for day 6

---
+++ Day 6/index.js
@@ -0,0 +1,18 @@
+// DAY 6 -- Wait For It
+// https://adventofcode.com/2023/day/6
+
+// As part of signing up, you get a sheet of paper (your puzzle input) that lists the time
+// allowed for each race and also the best distance ever recorded in that race.
+// To guarantee you win the grand prize, you need to make sure you go farther in each race than
+// the current record holder.
+
+// The boats are much smaller than you expected - they're actually toy boats, each with a big button
+// on top. Holding down the button charges the boat, and releasing the button allows the boat to move.
+// Boats move faster if their button was held longer, but time spent holding the button counts against
+// the total race time. You can only hold the button at the start of the race, and boats don't move until
+// the button is released.
+
+// For example:
+
+// Time:      7  15   30
+// Distance:  9  40  200
